d5ce32ee2c2e4260fc65a054390665e173b2e717
Camille Kempinaire <c.kempinaire@interface3.be>
2022-10-26 09:33:41 +0200

    jeu de carte ajouté

---
+++ hackaton_22/testHash.php
@@ -0,0 +1,8 @@
+<?php
+
+$password= "coucou";
+$passwordHash = password_hash($password,
+								PASSWORD_DEFAULT,
+								array ('cost'=>12));
+								
+echo "Le password hashé est: ".$passwordHash;		
