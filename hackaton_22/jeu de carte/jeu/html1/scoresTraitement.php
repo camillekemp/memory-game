5693df8ea8ffcb1cd28a81d46bb67081d3b4f8c0
Camille Kempinaire <c.kempinaire@interface3.be>
2022-10-26 15:08:57 +0200

    jeu

---
+++ hackaton_22/jeu de carte/jeu/html1/scoresTraitement.php
@@ -0,0 +1,8 @@
+<?php
+
+$score = $_GET['score'];
+
+echo json_encode("le score est" . $score);
+?>
+
+
---
+++ hackaton_22/jeu de carte/jeu/html1/scripthtml1.js
@@ -0,0 +1,94 @@
+const cards = document.querySelectorAll('.memory-card');
+const html_score = document.querySelector('.score');
+console.log(html_score.value);
+
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
+
+  this.classList.add('flip');
+
+  if (!hasFlippedCard) {
+    // first click
+    hasFlippedCard = true;
+    firstCard = this;
+
+    return;
+  }
+
+  // second click
+  secondCard = this;
+
+  checkForMatch();
+}
+
+function checkForMatch() {
+  let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+
+  isMatch ? disableCards() : unflipCards();
+  
+}
+
+function disableCards() {
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
+
+  // rajouter points
+ html_score.innerHTML = parseInt(html_score.innerHTML) + 1
+  console.log("victory");
+
+  //rajouter 1 point dans la base de données
+
+  let xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function(){
+   if (xhr.readyState == 4){
+     if (xhr.status == 200|| xhr.status == 304) {
+      let reponse = xhr.responseText;
+      html_score.innerText = reponse;
+     
+    }
+
+     else {
+      console.log("erreur")
+     }
+      
+  }
+};
+  xhr.open('GET',`./scoresTraitement.php?score=${html_score.innerText}`);
+  xhr.send();
+
+
+  resetBoard();
+}
+
+function unflipCards() {
+  lockBoard = true;
+  
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+
+    resetBoard();
+  }, 1000);
+}
+
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+  cards.forEach(card => {
+    let randomPos = Math.floor(Math.random() * 6);
+    card.style.order = randomPos;
+  });
+})();
+
+cards.forEach(card => card.addEventListener('click', flipCard));
+
+
+//function victory(){}
